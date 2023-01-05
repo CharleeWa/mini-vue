1eb916773aa459fe40b027588b547f70505ff36e
CharleeWa <18888351756@163.com>
2023-01-05 20:44:38 +0800

    实现初始化 component 主流程(#16)

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,44 @@
+export function createComponentInstance(vnode) {
+  const component = {
+    vnode,
+    type: vnode.type
+  }
+
+  return component
+}
+
+export function setupComponent(instance) {
+  // initProps()
+  // initSlots()
+  setupStatefulComponent(instance)
+}
+
+function setupStatefulComponent(instance: any) {
+  const Component = instance.type
+
+  const { setup } = Component
+
+  if(setup) {
+    // funcation object
+    const setupResult = setup()
+    handleSetupResult(instance ,setupResult)
+  }
+}
+
+function handleSetupResult(instance ,setupResult:any) {
+  // function object
+  // TODO function
+  if(typeof setupResult === 'object') {
+    instance.setupResult = setupResult
+  }
+
+  finishComponentSetup(instance)
+}
+
+function finishComponentSetup(instance: any) {
+  const Component = instance.type
+  if(Component.render) {
+    instance.render = Component.render
+  }
+}
+
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,3 +1,6 @@
+import { effect } from "../reactivity/effect"
+import { createComponentInstance, setupComponent } from "./component"
+
 export function render(vnode, container) {
   // patch
   patch(vnode, container)
@@ -5,4 +8,22 @@ export function render(vnode, container) {
 
 function patch(vnode, container) {
   // 去处理组件
+  processComponent(vnode, container)
 }
+
+function processComponent(vnode:any, container: any) {
+  mountComponent(vnode, container)
+}
+
+function mountComponent(vnode: any, container: any) {
+  const instance = createComponentInstance(vnode)
+  setupComponent(instance)
+  setupRenderEffect(instance, container)
+}
+
+function setupRenderEffect(instance: any, container: any) {
+  const subTree = instance.render()
+
+  patch(subTree, container)
+}
+
