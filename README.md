202d198d0fc075ed10f688a9606da6a29bcdddc9
CharleeWa <18888351756@163.com>
2023-01-06 16:46:53 +0800

    实现初始化 element 主流程(#18)

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 - ✔ 实现 computed 计算属性功能
 - ✔ 实现初始化 component 主流程
 - ✔ 使用 rollup 打包库
-- [ ] 实现初始化 element 主流程
+- ✔ 实现初始化 element 主流程
 - [ ] 实现组件代理对象(包含 this. api 的实现原理)
 - [ ] 实现 shapeFlags
 - [ ] 实现注册事件功能
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,4 +1,4 @@
-import { effect } from "../reactivity/effect"
+import { isObject } from "../shared/index"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
@@ -7,8 +7,11 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  // 去处理组件
-  processComponent(vnode, container)
+  if(typeof vnode.type === 'string') {
+    processElement(vnode, container)
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container)
+  }
 }
 
 function processComponent(vnode:any, container: any) {
@@ -27,3 +30,34 @@ function setupRenderEffect(instance: any, container: any) {
   patch(subTree, container)
 }
 
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container)
+}
+
+function mountElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type)
+
+  // string array
+  const { children } = vnode
+
+  if(typeof children === 'string') {
+    el.textContent = children
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el)
+  }
+  // props
+  const { props } = vnode
+
+  for (const key in props) {
+    const val = props[key]
+    el.setAttribute(key, val)
+  }
+
+  container.append(el)
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach(v => {
+    patch(v, container)
+  })
+}
