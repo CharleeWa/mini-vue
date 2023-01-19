56535928dbef46f2b9fcd19a63179ff1cfa7a2b0
CharleeWa <18888351756@163.com>
2023-01-19 22:15:26 +0800

    (wip) 更新 element 的 children - 双端对比diff 算法 （3）(视频:13.28)

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -187,6 +187,8 @@ export function createRenderer(options) {
       let patched = 0
       const toBePatched = e2 - s2 + 1
       const keyToNewIndexMap = new Map()
+      const newIndexToOldIndexMap = new Array(toBePatched)
+      for (let i = 0; i < toBePatched; i++) newIndexToOldIndexMap[i] = 0
 
       for (let i = s2; i <= e2; i++) {
         const nextChild = c2[i]
@@ -216,10 +218,15 @@ export function createRenderer(options) {
         if(newIndex === undefined) {
           hostRemove(prevChild.el)
         } else {
+          newIndexToOldIndexMap[newIndex - s2] = i + 1
           patch(prevChild, c2[newIndex], container, parentComponent, null)
           patched++
         }
       }
+
+      const increasingNewIndexSequence = getSequence(newIndexToOldIndexMap)
+
+      for (let i = 0; i < toBePatched; i++) {}
     }
   }
 
@@ -284,3 +291,43 @@ export function createRenderer(options) {
   }
 }
 
+function getSequence(arr) {
+  const p = arr.slice();
+  const result = [0];
+  let i, j, u, v, c;
+  const len = arr.length;
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i];
+    if (arrI !== 0) {
+      j = result[result.length - 1];
+      if (arr[j] < arrI) {
+        p[i] = j;
+        result.push(i);
+        continue;
+      }
+      u = 0;
+      v = result.length - 1;
+      while (u < v) {
+        c = (u + v) >> 1;
+        if (arr[result[c]] < arrI) {
+          u = c + 1;
+        } else {
+          v = c;
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1];
+        }
+        result[u] = i;
+      }
+    }
+  }
+  u = result.length;
+  v = result[u - 1];
+  while (u-- > 0) {
+    result[u] = v;
+    v = p[v];
+  }
+  return result;
+}
