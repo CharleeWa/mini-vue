591f60814f6bac09739301712e4da6bb510dbd44
CharleeWa <18888351756@163.com>
2023-01-20 16:13:41 +0800

    实现组件更新功能(#35)

--- README.md
+++ README.md
@@ -37,7 +37,7 @@
 - ✔ 更新 element 的 children - 双端对比diff 算法 （2）
 - ✔ 更新 element 的 children - 双端对比diff 算法 （3）
 - ✔ 学习尤大解决 bug 的处理方式
-- [ ] 实现组件更新功能
+- ✔ 实现组件更新功能
 - [ ] 实现 nextTick 功能
 - [ ] 实现解析插值功能
 - [ ] 实现解析 element 标签
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -2,6 +2,7 @@ import { effect } from "../reactivity/effect"
 import { EMPTY_OBJ } from "../shared"
 import { ShapeFlags } from "../shared/ShapeFlags"
 import { createComponentInstance, setupComponent } from "./component"
+import { shouldUpdateComponent } from "./componentUpdateUtils"
 import { createAppAPI } from "./createApp"
 import { Fragment, Text } from "./vnode"
 
@@ -51,17 +52,33 @@ export function createRenderer(options) {
   }
 
   function processComponent(n1, n2:any, container: any, parentComponent, anchor) {
-    mountComponent(n2, container, parentComponent, anchor)
+    if(!n1){
+      mountComponent(n2, container, parentComponent, anchor)
+    } else {
+      updateComponent(n1, n2)
+    }
+  }
+
+  function updateComponent(n1, n2) {
+    const instance = (n2.component = n1.component)
+
+    if(shouldUpdateComponent(n1, n2)) {
+      instance.next = n2
+      instance.update()
+    } else {
+      n2.el = n1.el
+      instance.vnode = n2
+    }
   }
 
   function mountComponent(initialVNode: any, container: any, parentComponent, anchor) {
-    const instance = createComponentInstance(initialVNode, parentComponent)
+    const instance = (initialVNode.component = createComponentInstance(initialVNode, parentComponent))
     setupComponent(instance)
     setupRenderEffect(instance, initialVNode, container, anchor)
   }
 
   function setupRenderEffect(instance: any, initialVNode, container, anchor) {
-    effect(() => {
+    instance.update = effect(() => {
       if(!instance.isMounted) {
         console.log('init')
         const { proxy } = instance
@@ -72,6 +89,12 @@ export function createRenderer(options) {
 
       } else {
         console.log("update")
+        const { next, vnode } = instance
+        if(next) {
+          next.el = vnode.el
+          updateComponentPreRender(instance, next)
+        }
+
         const { proxy } = instance
         const subTree = instance.render.call(proxy)
         const prevSubTree = instance.subTree
@@ -314,6 +337,12 @@ export function createRenderer(options) {
   return {
     createApp: createAppAPI(render) 
   }
+}
+
+function updateComponentPreRender(instance, nextVNode) {
+  instance.vnode = nextVNode.vnode
+  instance.next = null
+  instance.props = nextVNode.props
 }
 
 function getSequence(arr) {
