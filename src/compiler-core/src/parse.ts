58163e46ffc67f2f7dc442c310952ee38e4aaffd
CharleeWa <18888351756@163.com>
2023-01-26 16:54:09 +0800

    实现解析 element 标签(#39)

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast"
 
+const enum TagType {
+  Start,
+  End
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content)
   return createRoot(parseChildren(context))
@@ -23,9 +28,23 @@ function parseChildren(context) {
 }
 
 function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start)
+  parseTag(context, TagType.End)
+  return element
+}
+
+function parseTag(context:any, type: TagType) {
+  const match:any = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+
+  if(type === TagType.End) return
+  
   return {
     type: NodeTypes.ELEMENT,
-    tag: 'div'
+    tag
   }
 }
 
