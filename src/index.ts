57a81effc2749b4263abdc16f7c5a76cb6a29c79
CharleeWa <18888351756@163.com>
2023-02-02 14:00:25 +0800

    (wip)实现编译 template 成 render 函数(视频07:59)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export * from './runtime-dom'
 export * from './reactivity'
+
+import { baseCompile } from './compiler-core/src'
+
+function compileToFunction(template) {
+  const { code } = baseCompile(template)
+}
